490f5d5019aecd3a3db1a4a6735a8b7af408310d
Marco Túlio Castro <marcotc.al@gmail.com>
2018-04-02 20:35:47 +0100

    Add files via upload

--- tickets/templatetags/tickets_extras.py
+++ tickets/templatetags/tickets_extras.py
@@ -6,4 +6,7 @@ register = template.Library()
 @register.filter(name='encryptb64')
 def encryptb64(value):
 	return base64.b64encode(value.encode('utf8'))
-    
+        
+@register.filter(name='times')
+def times(number):
+	return range(1, number+1)   
